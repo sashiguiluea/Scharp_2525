2aef2e767851530e319b35a4701e82311402d224
SEGUNDO ANDRES SHIGUI LEMA <sa.shiguil@uea.edu.ec>
2025-06-04 23:41:01 -0500

    Primer commit

--- PracticaGit/obj/Debug/net9.0/PracticaGit.AssemblyInfo.cs
+++ PracticaGit/obj/Debug/net9.0/PracticaGit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75f617b91bf2f4654c7d99009f0e26b547c69e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
